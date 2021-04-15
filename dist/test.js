f81db1ddd2688fcc077e2e487c066703d1beb1a2
陆拾遗 <iwof920125@hotmail.com>
2021-04-15 09:21:05 +0800

    构建

--- dist/test.js
+++ dist/test.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var log4js_1 = require("./log4js");
+var log4ts_1 = require("./log4ts");
 var path_1 = require("path");
-var logger = log4js_1.default.getLogger(path_1.basename(__filename));
+var logger = log4ts_1.default.getLogger(path_1.basename(__filename));
 ///////////////////////////////////////////////////////
 logger.info("==========================程  序  启  动==========================");
 logger.debug("==========================程  序  启  动==========================");
