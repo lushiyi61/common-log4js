58151be22d75d029d2dbc35f6c66ae7386c62226
lushiyi <iwof920125@hotmail.com>
2021-11-06 14:44:43 +0800

    发布新版，增加了说明文档，升级了ts依赖版本

--- dist/log4ts.js
+++ dist/log4ts.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var log4js_1 = require("log4js");
 var logId = process.env.NODE_APP_INSTANCE ? process.env.NODE_APP_INSTANCE : "0";
 var logEnv = process.env.NODE_ENV ? process.env.NODE_ENV : "NULL";
-var log4js = log4js_1.configure({
+var log4js = (0, log4js_1.configure)({
     pm2: true,
     disableClustering: true,
     appenders: {
--- dist/test.js
+++ dist/test.js
@@ -2,7 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var log4ts_1 = require("./log4ts");
 var path_1 = require("path");
-var logger = log4ts_1.default.getLogger(path_1.basename(__filename));
+var logger = log4ts_1.default.getLogger((0, path_1.basename)(__filename));
 ///////////////////////////////////////////////////////
 logger.info("==========================程  序  启  动==========================");
 logger.debug("==========================程  序  启  动==========================");
